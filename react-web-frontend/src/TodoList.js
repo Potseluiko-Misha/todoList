515496189bbfe3abdd81283d8f9daad2e35c0485
Misha <vechnuignor@mail.ru>
2024-12-25 19:10:05 +0300

    changeTodolistJS

--- react-web-frontend/src/TodoList.js
+++ react-web-frontend/src/TodoList.js
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
 import { baseURL } from './utility.js';
 import TodoItem from './TodoItem.js';
 import TodoSearch from './TodoSearch.js';
 import TodoSort from './TodoSort.js';
+import { TokenContext, RefreshListContext } from './utility.js';
 
-export default function TodoList({ token }) {
+export default function TodoList() {
     const [todos, setTodos] = useState([]);
     const [search, setSearch] = useState('');
     const [sort, setSort] = useState('0');
+    const [refresh, setRefresh] = useState(false);
+    const token = useContext(TokenContext);
 
     function acceptSearch(searchWord) {
         setSearch(searchWord);
@@ -18,7 +22,12 @@ export default function TodoList({ token }) {
         setSort(sortOrder);
     }
 
+    function refreshList() {
+        setRefresh(!refresh);
+    }
+
     useEffect(() => {
+        if (!token) return;
         (async () => {
             const params = new URLSearchParams();
             if (search)
@@ -35,16 +44,24 @@ export default function TodoList({ token }) {
                 window.alert(response.status + ': ' +
                              response.statusText);
         })();
-    }, [token, search, sort]);
+    }, [token, search, sort, refresh]);
 
     return (
         <>
-            <TodoSearch search={search} acceptSearch={acceptSearch} />
-            <TodoSort sort={sort} acceptSort={acceptSort} />
-            <h1 className="heading">Запланированные дела</h1>
-            {todos.map((item) => (
-                <TodoItem key={item._id} item={item} />
-            ))}
+            {!token && <Navigate to="/login" />}
+            {token &&
+                <>
+                    <TodoSearch search={search}
+                                acceptSearch={acceptSearch} />
+                    <TodoSort sort={sort} acceptSort={acceptSort} />
+                    <h1 className="heading">Запланированные дела</h1>
+                    <RefreshListContext.Provider value={refreshList}>
+                        {todos.map((item) => (
+                            <TodoItem key={item._id} item={item} />
+                        ))}
+                    </RefreshListContext.Provider>
+                </>
+            }
         </>
     );
 }
